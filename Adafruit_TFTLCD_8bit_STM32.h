74787c5b9f784a495e060de36c00d38205081fc5
Mike Kushnerik <59262898+mkengineering@users.noreply.github.com>
2020-09-05 11:40:59 -0500

    Fixed GPIO clock initialization error.

--- Adafruit_TFTLCD_8bit_STM32.h
+++ Adafruit_TFTLCD_8bit_STM32.h
@@ -1,63 +1,58 @@
 // Graphics library by ladyada/adafruit with init code from Rossum
 // MIT license
-//ported to ST HAL based core by MKE
+// ported to STM32duino ST Core by MKE
 
 #ifndef _ADAFRUIT_TFTLCD_8BIT_STM32_H_
 #define _ADAFRUIT_TFTLCD_8BIT_STM32_H_
 
+#include "Arduino.h"
 #include <Adafruit_GFX.h>
 
-//#include <libmaple/gpio.h> //used for Roger Clark / Maple core
-
 /*****************************************************************************/
-// Define pins and Output Data Registers
+// Data and Control Port / Pin Definitions
 /*****************************************************************************/
-// Data port
-//For my project I used PA0-PA7 -> DB0-DB7
-#define TFT_DATA_PORT	GPIOA 
-// Data bits/pins
-#define TFT_DATA_SHIFT 0 // take the lower bits/pins 0..7
-//#define TFT_DATA_SHIFT 8 // take the higher bits/pins 8..15
 
-//Control pins |RD |WR |RS |CS |RST|
+// Example: PB5-> PORT = GPIOB, MASK = GPIO_PIN_5
+
+// Data and Control Ports
+
+#define TFT_DATA_PORT	GPIOA
 #define TFT_CNTRL_PORT	GPIOB
-#define TFT_RD			PB8
-#define TFT_WR			PB7
-#define TFT_RS			PB6
-#define TFT_CS			PB5
-//Masks - Example: PB5 -> GPIO_PIN_5
-//Note: using digitalPinToBitMask(PIN) slows display writes down substantially 
-#define TFT_RD_MASK		GPIO_PIN_8 // HAL mask for RD pin (PB8 in example)
-#define TFT_WR_MASK		GPIO_PIN_7
-#define TFT_RS_MASK		GPIO_PIN_6
-#define TFT_CS_MASK		GPIO_PIN_5
 
-#define TFT_RST			PB9 //can be any pin (doesn't need to be same port as cntrl pins)
+// Masks for Control Pins
 
-#define SLOW_WRITE 0   // set to 1 for legacy slow write (using individual digitalWrite()s
+#define TFT_RD_MASK		GPIO_PIN_9
+#define TFT_WR_MASK		GPIO_PIN_8
+#define TFT_RS_MASK		GPIO_PIN_7
+#define TFT_CS_MASK		GPIO_PIN_6
+#define TFT_RST_MASK	GPIO_PIN_5
 
 /*****************************************************************************/
-// LCD controller chip identifiers
-#define ID_932X    0
-#define ID_7575    1
-#define ID_9341    2
-#define ID_HX8357D    3
-#define ID_9338    4
-#define ID_UNKNOWN 0xFF
+// TFT Controller Chip
+/*****************************************************************************/
+
+#define ID_932X    		0
+#define ID_7575    		1
+#define ID_9341    		2
+#define ID_HX8357D 		3
+#define ID_9338    		4
+#define ID_UNKNOWN 		0xFF
 
 /*****************************************************************************/
+// TFT Size
+/*****************************************************************************/
+
 #define TFTWIDTH   240
 #define TFTHEIGHT  320
 
-// Initialization command tables for different LCD controllers
 #define TFTLCD_DELAY 0xFF
 
-// For compatibility with sketches written for older versions of library.
-// Color function name was changed to 'color565' for parity with 2.2" LCD
-// library.
 #define Color565 color565
 
 /*****************************************************************************/
+// Color Definitions
+/*****************************************************************************/
+
 #define	BLACK   0x0000
 #define	BLUE    0x001F
 #define	RED     0xF800
@@ -67,90 +62,43 @@
 #define YELLOW  0xFFE0
 #define WHITE   0xFFFF
 
+/*****************************************************************************/
+// Macro Definitions
+/*****************************************************************************/
 
-	#define RD_ACTIVE    digitalWrite(TFT_RD, LOW)
-	#define RD_IDLE      digitalWrite(TFT_RD, HIGH)
-#if SLOW_WRITE
-	// use old definition, standard bit toggling, low speed
-	#define WR_ACTIVE    digitalWrite(TFT_WR, LOW)
-	#define WR_IDLE      digitalWrite(TFT_WR, HIGH)
-	#define CD_COMMAND   digitalWrite(TFT_RS, LOW)
-	#define CD_DATA      digitalWrite(TFT_RS, HIGH)
-	#define CS_ACTIVE    digitalWrite(TFT_CS, LOW)
-	#define CS_IDLE      digitalWrite(TFT_CS, HIGH)
-	#define CS_ACTIVE_CD_COMMAND	{ CS_ACTIVE; CD_COMMAND; }
-#else
-	// use fast bit toggling, very fast speed!
-//extern gpio_reg_map * cntrlRegs; //Libmaple method
-extern GPIO_TypeDef * cntrlRegs; //HAL method
+#define RD_ACTIVE               { TFT_CNTRL_PORT->BRR  = TFT_RD_MASK; }
+#define RD_IDLE                 { TFT_CNTRL_PORT->BSRR = TFT_RD_MASK; }
 
-	#define WR_ACTIVE				{ cntrlRegs->BRR  = TFT_WR_MASK; }
-	#define WR_IDLE					{ cntrlRegs->BSRR = TFT_WR_MASK; }
-	#define CD_COMMAND				{ cntrlRegs->BRR  = TFT_RS_MASK; }
-	#define CD_DATA					{ cntrlRegs->BSRR = TFT_RS_MASK; }
-	#define CS_ACTIVE				{ cntrlRegs->BRR  = TFT_CS_MASK; }
-	#define CS_IDLE					{ cntrlRegs->BSRR = TFT_CS_MASK; }
-	#define CS_ACTIVE_CD_COMMAND	{ cntrlRegs->BRR  = (TFT_CS_MASK|TFT_RS_MASK); }
-#endif
+#define WR_ACTIVE				{ TFT_CNTRL_PORT->BRR  = TFT_WR_MASK; }
+#define WR_IDLE					{ TFT_CNTRL_PORT->BSRR = TFT_WR_MASK; }
+#define WR_STROBE 				{ WR_ACTIVE; WR_IDLE; }
 
-#define WR_STROBE { WR_ACTIVE; WR_IDLE; }
+#define CD_COMMAND				{ TFT_CNTRL_PORT->BRR  = TFT_RS_MASK; }
+#define CD_DATA					{ TFT_CNTRL_PORT->BSRR = TFT_RS_MASK; }
+
+#define CS_ACTIVE				{ TFT_CNTRL_PORT->BRR  = TFT_CS_MASK; }
+#define CS_IDLE					{ TFT_CNTRL_PORT->BSRR = TFT_CS_MASK; }
+#define CS_ACTIVE_CD_COMMAND	{ TFT_CNTRL_PORT->BRR  = (TFT_CS_MASK|TFT_RS_MASK); }
+
+#define RST_ACTIVE				{ TFT_CNTRL_PORT->BRR = TFT_RST_MASK; }
+#define RST_IDLE				{ TFT_CNTRL_PORT->BSRR = TFT_RST_MASK; }
+#define RST_TOGGLE				{ RST_IDLE; RST_ACTIVE; RST_IDLE; }
 
 extern uint8_t read8_(void);
+
 #define read8(x) ( x = read8_() )
 
-//extern gpio_reg_map * dataRegs;
-extern GPIO_TypeDef * dataRegs;
+// set control pins (PB5-PB9) to output mode and enable apb2 clock for gpiob
+#define setCntrlDir() { RCC->APB2ENR |= 0b1000; TFT_CNTRL_PORT->CRH = 0x00000033; TFT_CNTRL_PORT->CRL = 0x33300000; }
 
-#if (TFT_DATA_SHIFT==0)
-  //#warning "Using lower data nibble..."
-	// set the pins to input mode
-	#define setReadDir() ( dataRegs->CRL = 0x88888888 )	// set the lower 8 bits as input
-	// set the pins to output mode
-	#define setWriteDir() ( dataRegs->CRL = 0x33333333 )	// set the lower 8 bits as output
+// set the data pins (PA0-PA7) to input mode and enable apb2 clock for gpioa
+#define setReadDir() { RCC->APB2ENR |= 0b0100; TFT_DATA_PORT->CRL = 0x88888888; }
 
-    // set pins to output the 8 bit value
-    #if SLOW_WRITE
-     inline void write8(uint8_t c) { /*Serial.print(" write8: "); Serial.print(c,HEX); Serial.write(',');*/
-    					digitalWrite(PA0, (c&BIT0)?HIGH:LOW);
-    					digitalWrite(PA1, (c&BIT1)?HIGH:LOW);
-    					digitalWrite(PA2, (c&BIT2)?HIGH:LOW);
-    					digitalWrite(PA3, (c&BIT3)?HIGH:LOW);
-    					digitalWrite(PA4, (c&BIT4)?HIGH:LOW);
-    					digitalWrite(PA5, (c&BIT5)?HIGH:LOW);
-    					digitalWrite(PA6, (c&BIT6)?HIGH:LOW);
-    					digitalWrite(PA7, (c&BIT7)?HIGH:LOW);
-    					WR_STROBE; }
-    #else
-      #define write8(c) { dataRegs->BSRR = (uint32_t)(0x00FF0000 + ((c)&0xFF)); WR_STROBE; }
-      // inline void write8(uint8_t d) { dataRegs->BSRR = (uint32_t)(0x00FF0000 + d); WR_STROBE; };
-    #endif
-#elif (TFT_DATA_SHIFT==8)
-  #warning "Using high data nibble..."
-	// set the pins to input mode
-	#define setReadDir() ( dataRegs->CRH = 0x88888888 )	// set the upper 8 bits as input
-	// set the pins to output mode
-	#define setWriteDir() ( dataRegs->CRH = 0x33333333 )	// set the lower 8 bits as output
+// set the data pins (PA0-PA7) to output mode and enable apb2 clock for gpioa
+#define setWriteDir() { RCC->APB2ENR |= 0b0100; TFT_DATA_PORT->CRL = 0x33333333; }
 
-    // set pins to output the 8 bit value
-    #if SLOW_WRITE
-     inline void write8(uint8_t c) { /*Serial.print(" write8: "); Serial.print(c,HEX); Serial.write(',');*/
-    					digitalWrite(PA8, (c&BIT0)?HIGH:LOW);
-    					digitalWrite(PA9, (c&BIT1)?HIGH:LOW);
-    					digitalWrite(PA10, (c&BIT2)?HIGH:LOW);
-    					digitalWrite(PA11, (c&BIT3)?HIGH:LOW);
-    					digitalWrite(PA12, (c&BIT4)?HIGH:LOW);
-    					digitalWrite(PA13, (c&BIT5)?HIGH:LOW);
-    					digitalWrite(PA14, (c&BIT6)?HIGH:LOW);
-    					digitalWrite(PA15, (c&BIT7)?HIGH:LOW);
-    					WR_STROBE; }
-    #else
-      #define write8(c) {  dataRegs->BSRR = (uint32_t)(0xFF000000 + (((c)&0xFF)<<TFT_DATA_SHIFT)); WR_STROBE; }
-      // inline void write8(uint8_t d) { dataRegs->BSRR = (uint32_t)(0x00FF0000 + d); WR_STROBE; };
-    #endif
-#else
-  #error Invalid data shift selected! Please set to '0' for low nibble, or to '8' for high nibble
-#endif
-
+// set pins to output the 8 bit value
+#define write8(c) { TFT_DATA_PORT->BSRR = (uint32_t)(0x00FF0000 + ((c)&0xFF)); WR_STROBE; }
 
 /*****************************************************************************/
 
@@ -161,10 +109,9 @@ class Adafruit_TFTLCD_8bit_STM32 : public Adafruit_GFX {
 
  public:
 
-  //Adafruit_TFTLCD_8bit_STM32(uint8_t cs, uint8_t cd, uint8_t wr, uint8_t rd, uint8_t rst);
   Adafruit_TFTLCD_8bit_STM32(void);
 
-  void     begin(uint16_t id = 0x9328);
+  void     begin(uint16_t id = 0x9341);
   void     drawPixel(int16_t x, int16_t y, uint16_t color);
   void     drawFastHLine(int16_t x0, int16_t y0, int16_t w, uint16_t color);
   void     drawFastVLine(int16_t x0, int16_t y0, int16_t h, uint16_t color);
@@ -179,9 +126,7 @@ class Adafruit_TFTLCD_8bit_STM32 : public Adafruit_GFX {
   void     invertDisplay(boolean i),
 			pushColors(uint16_t *data, int16_t len, boolean first),
            drawBitmap(int16_t x, int16_t y, int16_t w, int16_t h, const uint16_t * bitmap);
-
-  uint16_t readPixel(int16_t x, int16_t y),
-           readID(void);
+  uint16_t readPixel(int16_t x, int16_t y), readID(void);
 /*****************************************************************************/
 // Pass 8-bit (each) R,G,B, get back 16-bit packed color
 // color coding on bits:
@@ -193,12 +138,7 @@ class Adafruit_TFTLCD_8bit_STM32 : public Adafruit_GFX {
   uint16_t inline color565(uint8_t r, uint8_t g, uint8_t b) { return ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3); }
 
  private:
-
-  void     init(),
-           // These items may have previously been defined as macros
-           // in pin_magic.h.  If not, function versions are declared:
-           //setLR(void),
-           flood(uint16_t color, uint32_t len);
+  void     init(), flood(uint16_t color, uint32_t len);
   uint8_t  driver;
 };
 
@@ -209,7 +149,6 @@ extern void writeRegister8(uint16_t a, uint8_t d);
 extern void writeRegister16(uint16_t a, uint16_t d);
 extern void writeRegister24(uint16_t a, uint32_t d);
 extern void writeRegister32(uint16_t a, uint32_t d);
-//extern void writeRegister32(uint16_t a, uint16_t d1, uint16_t d2);
 extern void writeRegisterPair(uint16_t aH, uint16_t aL, uint16_t d);
 
 extern Adafruit_TFTLCD_8bit_STM32 tft;
